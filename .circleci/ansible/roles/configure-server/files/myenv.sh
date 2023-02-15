2dd96bf152851142f0fffce9e8fa0490b492e1e0
Simon Gbafe <simon.gbafe@gmail.com>
2023-02-15 06:36:31 +0100

    smoke test

--- .circleci/ansible/roles/configure-server/files/myenv.sh
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -1,13 +1,9 @@
-NODE_ENV=prod
-VERSION=1
 TYPEORM_CONNECTION=postgres
 TYPEORM_MIGRATIONS_DIR=./src/migrations
 TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
 TYPEORM_MIGRATIONS=./src/migrations/*.ts
-
-# Things you can change if you wish...
-TYPEORM_HOST=localhost
-TYPEORM_PORT=5532
+TYPEORM_HOST=udapeople-prod.cqmouk4oh7jx.us-east-1.rds.amazonaws.com
+TYPEORM_PORT=5432
 TYPEORM_USERNAME=postgres
-TYPEORM_PASSWORD=password
-TYPEORM_DATABASE=glee
+TYPEORM_PASSWORD=udapeople123
+TYPEORM_DATABASE=postgree
